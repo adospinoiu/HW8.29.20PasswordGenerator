2ca8d9bd5b732ef4d129c5e84cb009ecf50407ea
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-08-22 21:59:56 -0700

    Added the following arrays: specialCharacter, numbers, lowerCase and upperCase

--- script.js
+++ script.js
@@ -20,7 +20,10 @@ function generatePassword(){
 
   // Password is currently blank! We need to make a better one
   let password = "";
-
+  let specialCharacter = [""", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^",_"`", "{", "|", "}", "~"];
+  let numbers = [0,1,2,3,4,5,6,7,8,9];
+  let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y, "Z"];
 
 
 

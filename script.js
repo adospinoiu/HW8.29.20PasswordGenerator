25f0e37efa53b33a7bbe1bd8dcad6bc96308e908
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-08-23 05:38:48 -0700

    Added while loop to evalaute passwordLength to ensure min: 8 and max 128

--- script.js
+++ script.js
@@ -19,11 +19,21 @@ function writePassword() {
 function generatePassword(){
 
   // Password is currently blank! We need to make a better one
-  let password = "";
-  let specialCharacter = [""", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^",_"`", "{", "|", "}", "~"];
-  let numbers = [0,1,2,3,4,5,6,7,8,9];
-  let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y, "Z"];
+  let password = "Hello";
+
+  let passwordLength = prompt("Enter the password length.\n(Min: 8 -- Max: 128 characters.");
+  
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Incorrect LENGTH entered. Enter a new password length.\n(Min: 8 -- Max: 128 characters.");
+  }
+  
+  console.log(passwordLength);
+
+
+  //let specialCharacter = [""", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^",_"`", "{", "|", "}", "~"];
+  //let numbers = [0,1,2,3,4,5,6,7,8,9];
+  //let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  //let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y, "Z"];
 
 
 
